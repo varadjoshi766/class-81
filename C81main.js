c620f42d47caff8384826803e8bd46ccb365b434
varadjoshi766 <72878877+varadjoshi766@users.noreply.github.com>
2021-03-10 21:06:46 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,23 @@
+canvas=document.getElementById("My_Canvas");
+ctx=canvas.getContext("2d");
+color= "green";
+ctx.beginPath();
+ctx.strokeStyle= color;
+ctx.lineWidth= 3;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e){
+    mouse_x= e.clientX-canvas.offsetLeft;
+    mouse_y= e.clientY-canvas.offsetTop;
+console.log("x= "+mouse_x+"y= "+mouse_y);
+circle(mouse_x,mouse_y);
+}
+ function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle= color;
+    ctx.lineWidth= 3;
+    ctx.arc(mouse_x, mouse_y, 40,0,2*Math.PI);
+    ctx.stroke();
+ }
